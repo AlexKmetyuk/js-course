ec82f5fec8b0ab5a1422b08a165e00c78aab35f8
Alexander Kmetyuk <kmetyuk.sasha@gmail.com>
2022-12-27 14:35:17 +0200

    save

--- js/script.js
+++ js/script.js
@@ -3,51 +3,51 @@
 
 // Для кнопки видалення - ╳
 
-const todosList = document.querySelector(".todos");
-const btn = document.querySelector(".add-btn");
-const input = document.querySelector(".input");
+// const todosList = document.querySelector(".todos");
+// const btn = document.querySelector(".add-btn");
+// const input = document.querySelector(".input");
 
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
 
-  if (!input.value) return;
+//   if (!input.value) return;
 
-  createTodo(input.value);
-  input.value = "";
-});
+//   createTodo(input.value);
+//   input.value = "";
+// });
 
-function createTodo(text) {
-  const item = document.createElement("li");
-  item.classList.add("todo");
+// function createTodo(text) {
+//   const item = document.createElement("li");
+//   item.classList.add("todo");
 
-  const textEl = document.createElement("p");
-  textEl.textContent = text;
+//   const textEl = document.createElement("p");
+//   textEl.textContent = text;
 
-  item.append(textEl);
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("delete-btn");
-  deleteBtn.textContent = "╳";
-  item.append(deleteBtn);
+//   item.append(textEl);
+//   const deleteBtn = document.createElement("button");
+//   deleteBtn.classList.add("delete-btn");
+//   deleteBtn.textContent = "╳";
+//   item.append(deleteBtn);
 
-  todosList.append(item);
-}
+//   todosList.append(item);
+// }
 
-todosList.addEventListener("click", (e) => {
-  if (e.target === e.currentTarget) {
-    console.log(true);
-    return;
-  }
-  console.log(e.target);
+// todosList.addEventListener("click", (e) => {
+//   if (e.target === e.currentTarget) {
+//     console.log(true);
+//     return;
+//   }
+//   console.log(e.target);
 
-  if (e.target.classList.contains("delete-btn")) {
-    e.target.parentNode.remove();
-  }
+//   if (e.target.classList.contains("delete-btn")) {
+//     e.target.parentNode.remove();
+//   }
 
-  if (e.target.classList.contains("todo")) {
-    if (e.target.classList.contains("checked")) {
-      e.target.classList.remove("checked");
-    } else {
-      e.target.classList.add("checked");
-    }
-  }
-});
+//   if (e.target.classList.contains("todo")) {
+//     if (e.target.classList.contains("checked")) {
+//       e.target.classList.remove("checked");
+//     } else {
+//       e.target.classList.add("checked");
+//     }
+//   }
+// });
